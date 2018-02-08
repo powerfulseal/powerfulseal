cdf35453cf44ca7ff3b8983c0407f009fc053679
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2018-02-08 06:46:45 -0500

    added a test for the create_node_from_server function
    Signed-off-by: Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>

--- tests/clouddrivers/test_openstack.py
+++ tests/clouddrivers/test_openstack.py
@@ -3,7 +3,9 @@ import pytest
 
 from powerfulseal.clouddrivers.open_stack_driver import (
     get_all_ips,
+    create_node_from_server,
 )
+from powerfulseal.node import NodeState
 
 
 @pytest.fixture
@@ -15,6 +17,10 @@ def example_servers():
         group_no_addr=[dict()],
         empty_group=[],
     )
+    server1.id = "some_id"
+    server1.availability_zone = "DC1"
+    server1.name = "random server"
+    server1.status = "ACTIVE"
     return [server1]
 
 
@@ -22,3 +28,12 @@ def test_get_all_ips(example_servers):
     server = example_servers[0]
     res = get_all_ips(server)
     assert res == ["1.2.3.4", "11.22.33.44"]
+
+def test_create_node_from_server(example_servers):
+    server = example_servers[0]
+    node = create_node_from_server(server)
+    assert node.id == server.id
+    assert node.ip == server.addresses["group2"][0]["addr"]
+    assert node.az == server.availability_zone
+    assert node.name == server.name
+    assert node.state == NodeState.UP

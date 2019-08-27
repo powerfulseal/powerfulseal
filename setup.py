07997fb1d370f6de8a60b720528913fa40ee6900
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2019-08-27 17:27:52 +0100

    Patch version bump
    Signed-off-by: Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name='powerfulseal',
-    version='2.4.0',
+    version='2.4.1',
     author='Mikolaj Pawlikowski',
     author_email='mikolaj@pawlikowski.pl',
     url='https://github.com/bloomberg/powerfulseal',

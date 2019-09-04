ecf9548be1d8d634b8b39719418b07c5e1b03a26
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2019-09-04 17:33:31 +0100

    Set the long description content type to markdown
    Signed-off-by: Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     license=read('LICENSE'),
     description='PowerfulSeal - a powerful testing tool for Kubernetes clusters',
     long_description=read('README.md'),
+    long_description_content_type="text/markdown",
     install_requires=[
         'ConfigArgParse>=0.11.0,<1',
         'Flask>=1.0.0,<2',

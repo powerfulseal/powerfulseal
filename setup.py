ae6ab15250d3a9e4c89dc9ba58e5c4b99618de6d
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2019-10-08 07:55:08 +0100

    Revert "Don't use the long markdown description in setup.py"
    This reverts commit 735e945abcb276730fddc0dd7eb6016842e386d5.
    
    Signed-off-by: Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>

--- setup.py
+++ setup.py
@@ -4,12 +4,6 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-long_description = """
-**PowerfulSeal** adds chaos to your Kubernetes clusters, so that you can detect problems in your systems as early as possible. It kills targeted pods and takes VMs up and down.
-
-Please see the documentation at https://github.com/bloomberg/powerfulseal
-"""
-
 setup(
     name='powerfulseal',
     version='2.6.0',
@@ -19,7 +13,7 @@ setup(
     packages=find_packages(),
     license=read('LICENSE'),
     description='PowerfulSeal - a powerful testing tool for Kubernetes clusters',
-    long_description=long_description,
+    long_description=read('README.md'),
     #long_description_content_type="text/markdown",
     install_requires=[
         'ConfigArgParse>=0.11.0,<1',

eb18aa8252e07d188da574e97a7cb415e9ec23b6
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-05-11 19:55:56 +0100

    Fix ctrl-c
    Signed-off-by: Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>

--- powerfulseal/web/server.py
+++ powerfulseal/web/server.py
@@ -57,7 +57,7 @@ def start_server(host, port, policy, accept_proxy_headers=False, logger=None):
         app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1)
     config["policy"] = policy
     config["logger"] = logger
-    threading.Thread(target=app.run, args=(host, port)).start()
+    threading.Thread(target=app.run, args=(host, port), daemon=True).start()
 
 
 class ServerStateLogHandler(logging.Handler):

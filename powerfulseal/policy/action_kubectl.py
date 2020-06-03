43b5576d198cf2ec5326e97f6d652c330d851338
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-06-03 14:39:28 +0100

    Fix the kubectl action cleanup job
    Signed-off-by: Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>

--- powerfulseal/policy/action_kubectl.py
+++ powerfulseal/policy/action_kubectl.py
@@ -34,9 +34,9 @@ class ActionKubectl(ActionAbstract):
 
     def get_cleanup_actions(self):
         actions = []
-        if str(self.schema.get("autoDelete", "true")).lower() != "false":
+        if str(self.schema.get("autoDelete", True)).lower() != "false":
             delete_action = copy.deepcopy(self)
-            delete_action.schema["autoDelete"] = "false"
+            delete_action.schema["autoDelete"] = False
             delete_action.schema["action"] = "delete"
             actions.append(delete_action)
         return actions

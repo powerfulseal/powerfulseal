22b8c29e413952ebfbde3344551bf557be85efda
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-06-08 13:23:28 +0100

    Add a way to extract examples from the docs
    Signed-off-by: Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>

---
+++ docs/extract-examples.py
@@ -0,0 +1,45 @@
+import os 
+
+EXAMPLES_DIR = "./examples"
+EXAMPLE_NAME_TEMPLATE = "example-policy{}.yml"
+VALID_BEGINNGINGS = ["scenarios", "config"]
+
+cwd = os.path.dirname(os.path.realpath(__file__))
+
+
+f = []
+for (dirpath, dirnames, filenames) in os.walk(cwd):
+  for filename in filenames:
+    if filename.endswith(".md"):
+      print(dirpath + "/" + filename)
+      f.append(dirpath + "/" + filename)
+
+print(f)
+
+
+counter = 0
+for filename in f:
+  print("Reading " + filename)
+  with open(filename) as f:
+    content = ""
+    inside = False
+    for line in f.readlines():
+      if line.startswith("```yaml"):
+        print("Starting " + line)
+        inside = True
+      elif line.startswith("```"):
+        is_valid = False
+        for beg in VALID_BEGINNGINGS:
+          if content.startswith(beg):
+            is_valid = True
+        if is_valid and inside:
+          print("Finishing " + line)
+          output = EXAMPLES_DIR + EXAMPLE_NAME_TEMPLATE.format(counter)
+          print("Writing " + output)
+          with open(output, "w") as policy_file:
+            policy_file.write(content)
+          inside = False
+          content = ""
+          counter += 1
+      elif inside:
+        content += line

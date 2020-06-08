4838b6f6bfaf960c5612f6b1a57179217b3167a6
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-06-08 20:34:26 +0100

    Add make validate-examples
    Signed-off-by: Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>

--- docs/extract-examples.py
+++ docs/extract-examples.py
@@ -1,31 +1,28 @@
 import os 
 
-EXAMPLES_DIR = "./examples"
+EXAMPLES_DIR = "./examples/"
 EXAMPLE_NAME_TEMPLATE = "example-policy{}.yml"
 VALID_BEGINNGINGS = ["scenarios", "config"]
 
 cwd = os.path.dirname(os.path.realpath(__file__))
 
 
-f = []
+files = []
 for (dirpath, dirnames, filenames) in os.walk(cwd):
   for filename in filenames:
     if filename.endswith(".md"):
-      print(dirpath + "/" + filename)
-      f.append(dirpath + "/" + filename)
-
-print(f)
-
+      #print(dirpath + "/" + filename)
+      files.append(dirpath + "/" + filename)
 
 counter = 0
-for filename in f:
-  print("Reading " + filename)
+for filename in files:
+  #print("Reading " + filename)
   with open(filename) as f:
     content = ""
     inside = False
     for line in f.readlines():
       if line.startswith("```yaml"):
-        print("Starting " + line)
+        #print("Starting " + line)
         inside = True
       elif line.startswith("```"):
         is_valid = False
@@ -33,9 +30,9 @@ for filename in f:
           if content.startswith(beg):
             is_valid = True
         if is_valid and inside:
-          print("Finishing " + line)
+          #print("Finishing " + line)
           output = EXAMPLES_DIR + EXAMPLE_NAME_TEMPLATE.format(counter)
-          print("Writing " + output)
+          print(output)
           with open(output, "w") as policy_file:
             policy_file.write(content)
           inside = False
